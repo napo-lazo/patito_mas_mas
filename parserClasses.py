3e6a0c2d0ef98046b80922212471990f35b96b6b
Katia Tarin <45047009+ktarin@users.noreply.github.com>
2020-05-11 12:23:14 -0500

    added variable counters

---
+++ parserClasses.py
@@ -0,0 +1,61 @@
+# variablesTable guarda las variables globales y de las funciones
+class VariablesTable(object):
+
+    def __init__(self):
+        #variablesTable tiene el formato de {nombreScope : {returnType : valor, variables : {nombreVar1 : {type : valor, value : valor}, nombreVar2 : {type : valor, value : valor}, etc}}} 
+        self.variablesTable = {}
+        self.currentScope = None
+        self.currentType = None
+        self.currentId = None
+
+    def scopeExists(self, scopeName):
+        return self.variablesTable[scopeName]
+
+    def createScope(self, scopeName, returnType):
+        if scopeName == 'global':
+            self.variablesTable[scopeName] = {'returnType': returnType, 'variables' : {}}
+        else:
+            self.variablesTable[scopeName] = {'returnType': returnType, 'parameters' : [], 'variables' : {}}
+
+    def createVariable(self, variableName):
+        self.variablesTable[self.currentScope]['variables'][variableName] = {'type' : self.currentType}
+    
+    def variableExists(self, variableName):
+        return self.variablesTable[self.currentScope]['variables'][variableName]
+    
+    def currentVariableValue(self):
+        return self.variablesTable[self.currentScope]['variables'][self.currentId]['value']
+    
+    def assignValueToCurrentVariable(self, value):
+        self.variablesTable[self.currentScope]['variables'][self.currentId]['value'] = value
+
+    def getTypeOfVariable(self, variableName):
+        print(f'{variableName} del scope {self.currentScope}')
+        if self.currentScope is None: 
+            return self.variablesTable['global']['variables'][variableName]['type']
+        else:
+            try: 
+                return self.variablesTable[self.currentScope]['variables'][variableName]['type']
+            except:
+                return self.variablesTable['global']['variables'][variableName]['type']
+
+    def addParameterToList(self, paramName, paramType):
+            self.variablesTable[self.currentScope]['parameters'].append(paramType)
+            self.variablesTable[self.currentScope]['variables'][paramName] = {'type' : paramType}
+
+class FunctionDirectory(object):
+    # funcDir = {'nameid': {'returnType': <datatype> , 'parameters': {type: <tipo>, name: <nombre>},'varTable': <table>}}
+    def __init__(self):
+        self.functionDirectory={}
+        self.parameterCount = 0
+        self.localVariablesCount = 0
+        self.tempVariablesCount = 0
+        # Con este defines en donde empieza la funcion
+        self.quadrupleCounter = 0
+
+    def createFunction(self, functionName, returnType, parameters):
+        self.functionDirectory[functionName] = {'returnType': returnType, 'parameters': [], 'varTable': VariablesTable()}
+    
+    def releaseVars(self):
+        # release vartable, end function, update temporal var count
+        self.functionDirectory['varTable'].clear()
